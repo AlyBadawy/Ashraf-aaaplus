5628e0e5eeb0f306e6a46f9c37a948645668236f
Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>
2025-07-23 20:15:18 -0400

    META tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,33 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { NameAndLogo } from '@/components/shared/NameAndLogo';
-import { HeaderNav } from '@/components/nav/HeaderNav';
-import { Footer } from '@/components/footer/Footer';
-import Link from 'next/link';
-import { GoogleAnalytics } from '@next/third-parties/google';
+import "./globals.css";
+import type { Metadata } from "next";
+import { NameAndLogo } from "@/components/shared/NameAndLogo";
+import { HeaderNav } from "@/components/nav/HeaderNav";
+import { Footer } from "@/components/footer/Footer";
+import Link from "next/link";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
-  title: 'AAA PLUS Financial Services',
+  title: "AAA PLUS Financial Services",
   description:
-    'AAA Plus Financial Services, based in Orlando, FL, offers expert tax preparation, bookkeeping, and financial consulting. Led by Ashraf Abeltawab, CPA, with over two decades of experience, we provide personalized, accurate, and client-focused financial solutions. Discover trustworthy financial guidance for individuals and small businesses in the Orlando area.',
+    "Expert tax, bookkeeping, and financial consulting in Orlando, FL. Led by CPA Ashraf Abeltawab with 20+ years of trusted, client-focused service.",
   keywords: [
-    'Tax Preparation Orlando',
-    'Bookkeeping Services Orlando',
-    'Financial Consultation Orlando',
-    'Orlando CPA',
-    'Nonprofit Accounting Orlando',
-    'خدمات الضرائب أورلاندو',
-    'خدمات المحاسبة أورلاندو',
-    'استشارات مالية أورلاندو',
-    'محاسب معتمد أورلاندو',
-    'محاسبة الجمعيات غير الربحية أورلاندو',
+    "AAA PLUS Financial Services",
+    "Tax Services Orlando",
+    "AAA Plus Tax",
+    "AAA Plus Bookkeeping",
+    "AAA Plus Financial Consulting",
+    "AAA Plus CPA",
+    "AAA Plus Nonprofit Accounting",
+    "Tax Preparation Orlando",
+    "Bookkeeping Services Orlando",
+    "Financial Consultation Orlando",
+    "Orlando CPA",
+    "Nonprofit Accounting Orlando",
+    "خدمات الضرائب أورلاندو",
+    "خدمات المحاسبة أورلاندو",
+    "استشارات مالية أورلاندو",
+    "محاسب معتمد أورلاندو",
+    "محاسبة الجمعيات غير الربحية أورلاندو",
   ],
 };
 
@@ -30,23 +37,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className='antialiased'>
-        <div className='flex flex-col justify-between min-h-screen'>
+    <html lang="en">
+      <body className="antialiased">
+        <div className="flex flex-col justify-between min-h-screen">
           <div>
-            <header className='container flex flex-col sm:flex-row align-middle items-center justify-between py-6 mb-8 mx-auto main-header px-2'>
-              <Link href='/'>
+            <header className="container flex flex-col sm:flex-row align-middle items-center justify-between py-6 mb-8 mx-auto main-header px-2">
+              <Link href="/">
                 <NameAndLogo />
               </Link>
               <HeaderNav />
             </header>
-            <div className='px-2'>{children}</div>
+            <div className="px-2">{children}</div>
           </div>
           <Footer />
         </div>
       </body>
-      <GoogleAnalytics gaId='G-FSZLV457E9' />
-      <GoogleAnalytics gaId='AW-16787609825' />
+      <GoogleAnalytics gaId="G-FSZLV457E9" />
+      <GoogleAnalytics gaId="AW-16787609825" />
     </html>
   );
 }
--- src/components/footer/FooterAddress.tsx
+++ src/components/footer/FooterAddress.tsx
@@ -1,16 +1,21 @@
 export const FooterAddress = () => {
   return (
-    <div className='w-full md:w-1/4 mb-4 md:mb-0'>
-      <p className='text-sm'>
-        <span className='font-bold'>Phone: </span> (407) 726-7901
+    <div className="w-full md:w-1/4 mb-4 md:mb-0">
+      <p className="text-sm">
+        <span className="font-bold">Phone: </span> (407) 726-7901
       </p>
 
-      <p className='text-sm'>
-        <span className='font-bold'>Email:</span> contact@aaaplustax.com
+      <p className="text-sm">
+        <span className="font-bold">Email:</span> contact@aaaplustax.com
       </p>
-      <p className='text-sm'>
-        <span className='font-bold'>Address: </span>4700 Millenia Blvd. <br />
-        Orlando, FL 32839 - Suite 500
+      <p className="text-sm">
+        <span className="font-bold">
+          Address:
+          <address>
+            13732 Eden Isle Blvd <br />
+            Windermere, FL - 34786
+          </address>
+        </span>
       </p>
     </div>
   );
